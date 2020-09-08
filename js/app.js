801468e75cb42053cca37a0909fa84d7a44ac8ba
Alaahayek25 <alaalhayek25@gmail.com>
2020-09-08 20:26:53 +0300

    Edit

--- js/app.js
+++ js/app.js
@@ -1,55 +1,57 @@
 'use instrict';
+var name=prompt('what is your name?');
+alert('welcome here '+name);
 var q1 = prompt('is my major acounting?').toLowerCase();
 if (q1 == 'yes' || q1 == 'y') {
     //console.log('correct');
-    alert('correct');
+    alert('correct '+name);
 
 } else if (q1 == 'no' || q1 == 'n') {
     //console.log('sorry it\'s wrong');
-    alert('sorry it\'s wrong');
+    alert('sorry it\'s wrong '+name);
 } else {
-    alert('wrong input');
+    alert('wrong input '+name);
 }
 var q2 = prompt('did I work as a manager in acompany?').toLowerCase();
 if (q2=='yes' || q2=='y') {
     //console.log('sorry\worng');
-    alert('sorry\worng')
+    alert('sorry worng '+name)
 } else if (q2=='no' || q2=='n') {
-    alert('correct')
+    alert('correct '+name);
 }
 else {
-    alert('wrong input')
+    alert('wrong input '+name);
 }
 var q3 = prompt('did I work for restaurants?').toLowerCase();
 if (q3=='yes' || q3=='y'){
 
 //console.log('correct');
-alert('correct');
+alert('correct '+name);
 }else if (q3=='no' || q3 == 'n') {
 
     //console.log('sorry\worng');
-    alert('sorry\worng');
+    alert('sorry worng '+name);
 } else {
-    alert('wrong input');
+    alert('wrong input '+name);
 }
 var q4 =prompt('is I want travel io Canada?').toLowerCase();
 if ( q4=='yes'||q4=='y'){
     //console.log('correct');
-    alert('correct');
+    alert('correct '+name);
 } else if (q4=='no'||q4=='n'){
-alert('sorry\worng');}
+alert('sorry worng '+name);}
 else{ 
-    alert('wrong input');
+    alert('wrong input '+name);
 }
 var q5 =prompt('I worked in sales field?').toUpperCase();
 if(q5=='YES'||q5=='Y'){
    // console.log('correct')
-   alert('correct');
+   alert('correct '+name);
 }else if (q5=='NO'||q5=='N'){
     //console.log('incorrect');
-    alert('incorrect');
+    alert('incorrect '+name );
 }else {
-    alert('wrong input')
+    alert('wrong input '+name);
 }
 
 
